750a85878e8da715c06efa379516bf45468adf57
Ashking07 <ashwinkapile2002@gmail.com>
2023-07-18 22:02:10 +0530

    .

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -26,9 +26,11 @@ export default function TextForm(props) {
     <div>
       
 <div className="mb-3">
+
   <label htmlFor="myBox" className="form-label">{props.heading}</label>
-  <textarea className="form-control text-center" value={Text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
-  {/* button.btn.btn-primary     */}
+  
+  <textarea className="form-control text-center" value={Text} onChange={handleOnChange} id="myBox" rows="8"/>
+
     <button className="btn btn-primary my-3" onClick={handleUpperCaseClick}>Convert To UPPERCASE</button>
 </div>
     </div>
